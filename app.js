84ab321d73b0e3a36bf2357c71b3c5f718b76183
pungaymarkclaude-cell <pungaymarkclaude@gmail.com>
2026-04-26 17:44:51 +0800

    Add files via upload

---
+++ app.js
@@ -0,0 +1,7 @@
+function postAnnouncement() {
+    // Get input value
+    var message = document.getElementById("messageInput").value;
+
+    // Display it in output box
+    document.getElementById("outputBox").innerText = message;
+}
